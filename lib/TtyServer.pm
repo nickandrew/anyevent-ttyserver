a7397990b51bc2398106dbf31f9f177eed81d450
Nick Andrew <nick@nick-andrew.net>
2014-11-01 02:27:05 +1100

    Use TtyServer::Stream for line()/json()
    Gets rid of a lot of duplicate code and changes the API accordingly
    (removing $w arg; can use line() or json() but not both).

--- lib/TtyServer.pm
+++ lib/TtyServer.pm
@@ -65,77 +65,14 @@ sub stop {
 	$self->{cv}->send($rc);
 }
 
-sub _inputwait {
-	my ($self) = @_;
-
-	return if ($self->{input});
-
-	my $w;
-	$w = AnyEvent->io(
-		fh => \*STDIN,
-		poll => 'r',
-		cb => sub {
-			my $line = <STDIN>;
-			if (!defined $line) {
-				undef $w;
-				delete $self->{input};
-				$self->_distribute(undef, undef);
-				return;
-			}
-			$self->_distribute($w, $line);
-		}
-	);
-
-	$self->{input} = $w;
-}
-
-sub _distribute {
-	my ($self, $w, $line) = @_;
-
-	foreach my $lr (@{$self->{distributors}}) {
-		my ($func, $cb) = @$lr;
-		last if ($func->($self, $w, $line, $cb));
-	}
-}
-
-sub _line {
-	my ($self, $w, $line, $cb) = @_;
-
-	$cb->($self, $w, $line);
-	return 1;
-}
-
-sub _json {
-	my ($self, $w, $line, $cb) = @_;
-
-	if (!defined $line) {
-		$cb->($self, $w, undef);
-		return 1;
-	}
-
-	my $msg = eval { JSON::decode_json($line); };
-
-	if ($@) {
-		my $e = $@;
-		$self->{on_error}->("JSON decode error - $e", $line) if ($self->{on_error});
-	} else {
-		$cb->($self, $w, $msg);
-		return 1;
-	}
-
-	return 0;
-}
-
 sub line {
 	my ($self, $cb) = @_;
-	$self->_inputwait();
-	push(@{$self->{distributors}}, [ \&_line, $cb ]);
+	TtyServer::Stream->new($self, \*STDIN)->line($cb);
 }
 
 sub json {
 	my ($self, $cb) = @_;
-	$self->_inputwait();
-	push(@{$self->{distributors}}, [ \&_json, $cb ]);
+	TtyServer::Stream->new($self, \*STDIN)->json($cb);
 }
 
 sub send {

a4f0672ceb6a85a420d8a753659d2a4f02ce92f0
Nick Andrew <nick@nick-andrew.net>
2014-11-01 01:54:47 +1100

    Add simple shell client

---
+++ client/shell.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+#
+#  Shell client.
+#  Type a line, see it executed by the shell.
+
+use strict;
+use warnings;
+
+use TtyServer;
+use IPC::Open2;
+
+my ($child_out, $child_in);
+my $pid = open2($child_out, $child_in, "bin/shell.pl");
+
+my $stream_in = app->stream($child_in);
+
+line sub {
+	my ($self, $w, $line) = @_;
+
+	if (!defined $line) {
+		# EOF
+		print STDERR "EOF, stopping.\n";
+		app->stop(0);
+		return;
+	}
+
+	chomp($line);
+	$stream_in->send({cmd => $line});
+};
+
+app->stream($child_out)->json(sub {
+	my ($self, $w, $msg) = @_;
+
+	if (!defined $msg) {
+		print STDERR "$0 json EOF from peer, stopping.\n";
+		app->stop(0);
+		return;
+	}
+
+	if (ref($msg) eq 'HASH') {
+		printf("rc: %d\n", $msg->{rc});
+		print $msg->{stdout};
+	}
+});
+
+app->start;
